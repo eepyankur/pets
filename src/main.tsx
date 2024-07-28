dac8187bbc8f21027afa0ff7395c9b58bef8ca8d
ankur. <ankurjoshi2105@gmail.com>
2024-07-28 21:15:42 +0530

    added vercel web analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import { Providers } from "./utils/Providers.tsx";
 import App from "./App.tsx";
 import "./index.css";
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <Providers>
       <App />
     </Providers>
+    <Analytics />
   </React.StrictMode>,
 );
